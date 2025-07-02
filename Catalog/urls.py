523a6cf1105fe2f6b602f2a4a9dd48dcebc6784d
vladimir.kukushkin <enot.voriska.0.0.1@gmail.com>
2025-07-02 13:54:57 +0300

    ну-ну

--- Catalog/urls.py
+++ Catalog/urls.py
@@ -4,4 +4,5 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('', views.main, name='index'),
+    path('resume/', views.resume, name='resume'),
 ]
--- Catalog/views.py
+++ Catalog/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def main(request):
     return render (request, 'Catalog/main.html')
+
+def resume(request):
+    return render (request, 'Catalog/resume.html')
